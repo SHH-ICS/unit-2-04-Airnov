7f04274c6e3fcf91916b740183bb9e975d62b97a
Airnov <124378651+Airnov@users.noreply.github.com>
2023-03-08 10:07:37 -0500

    E

--- program.py
+++ program.py
@@ -5,9 +5,20 @@
 # For multiples of 5, output   Buzz
 # For multiples of 15, output   FizzBuzz
 # End at 32
+# I watced the extra video
+
+def fizzbuzz(i:int):
+  if i % 15 == 0:
+      return "FizzBuzz"
+  elif i % 3 == 0:
+      return "Fizz"
+  elif i % 5 == 0:
+      return "Buzz"
+  else:
+      return i
 
 result = ""
 for myNumber in range(32):
-  result = str(result) + str(myNumber) + "\n"
+  result = str(result) + str(fizzbuzz(myNumber)) + "\n"
 
 print(result)
